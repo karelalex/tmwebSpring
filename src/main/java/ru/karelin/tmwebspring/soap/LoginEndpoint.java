64fa4d6da09c189fabe3318c9f2873bd4fbdd5e2
karelalex <karelalex83@gmail.com>
2019-05-31 20:22:31 +0300

    soap Login works

--- src/main/java/ru/karelin/tmwebspring/soap/LoginEndpoint.java
+++ src/main/java/ru/karelin/tmwebspring/soap/LoginEndpoint.java
@@ -3,7 +3,13 @@ package ru.karelin.tmwebspring.soap;
 import org.apache.cxf.message.Message;
 import org.apache.cxf.phase.PhaseInterceptorChain;
 import org.apache.cxf.transport.http.AbstractHTTPDestination;
+import org.jetbrains.annotations.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import ru.karelin.tmwebspring.dto.Result;
 import ru.karelin.tmwebspring.entity.User;
 import ru.karelin.tmwebspring.service.UserService;
 
@@ -19,20 +25,29 @@ public class LoginEndpoint {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @WebMethod
     public String singIn(
             @WebParam(name = "login") String login,
             @WebParam(name = "password") String password
     ) {
-
-        User user = userService.findByLoginAndPassword(login, password);
-        if (user != null) {
-            Message message = PhaseInterceptorChain.getCurrentMessage();
-            HttpServletRequest request = (HttpServletRequest) message.get(AbstractHTTPDestination.HTTP_REQUEST);
-            HttpSession session = request.getSession(true);
-            session.setAttribute("userId", user.getId());
+        try {
+            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(login, password);
+            Authentication authentication = authenticationManager.authenticate(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            @Nullable final User user = userService.findByLogin(authentication.getName());
+            if (user != null) {
+                Message message = PhaseInterceptorChain.getCurrentMessage();
+                HttpServletRequest request = (HttpServletRequest) message.get(AbstractHTTPDestination.HTTP_REQUEST);
+                HttpSession session = request.getSession(true);
+                session.setAttribute("userId", user.getId());
+            }
             return user.getId();
-        } else return null;
+        } catch (Exception e) {
+            return null;
+        }
     }
 
 }
